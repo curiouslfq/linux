d6bd875dde9a0c6ff60c401d7ef8b0f311fa16c9
Lu Fengqi <lufq.fnst@cn.fujitsu.com>
2018-01-09 18:36:06 +0800

    backup_csum_root
    Signed-off-by: Lu Fengqi <lufq.fnst@cn.fujitsu.com>

--- fs/btrfs/ctree.h
+++ fs/btrfs/ctree.h
@@ -2180,7 +2180,7 @@ BTRFS_SETGET_STACK_FUNCS(stack_backup_dev_root_gen, struct btrfs_root_backup,
 BTRFS_SETGET_STACK_FUNCS(stack_backup_dev_root_level, struct btrfs_root_backup,
 		   dev_root_level, 8);
 
-BTRFS_SETGET_STACK_FUNCS(backup_csum_root, struct btrfs_root_backup,
+BTRFS_SETGET_STACK_FUNCS(stack_backup_csum_root, struct btrfs_root_backup,
 		   csum_root, 64);
 BTRFS_SETGET_STACK_FUNCS(backup_csum_root_gen, struct btrfs_root_backup,
 		   csum_root_gen, 64);
