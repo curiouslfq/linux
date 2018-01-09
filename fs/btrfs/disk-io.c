1939e0d757bf31c0a05aa168a60854d5483ea43e
Lu Fengqi <lufq.fnst@cn.fujitsu.com>
2018-01-09 18:26:30 +0800

    backup_chunk_root_level
    Signed-off-by: Lu Fengqi <lufq.fnst@cn.fujitsu.com>

--- fs/btrfs/disk-io.c
+++ fs/btrfs/disk-io.c
@@ -1922,7 +1922,7 @@ static void backup_super_roots(struct btrfs_fs_info *info)
 	btrfs_set_stack_backup_chunk_root(root_backup, info->chunk_root->node->start);
 	btrfs_set_stack_backup_chunk_root_gen(root_backup,
 			       btrfs_header_generation(info->chunk_root->node));
-	btrfs_set_backup_chunk_root_level(root_backup,
+	btrfs_set_stack_backup_chunk_root_level(root_backup,
 			       btrfs_header_level(info->chunk_root->node));
 
 	btrfs_set_backup_extent_root(root_backup, info->extent_root->node->start);
